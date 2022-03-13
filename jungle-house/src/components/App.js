9282d0153b48d656b3afd85358092c9584f5169e
fadoua <fadelhannachi@gmail.com>
2022-03-13 21:01:06 +0100

    css, logo ajoutés

--- jungle-house/src/components/App.js
+++ jungle-house/src/components/App.js
@@ -1,6 +1,6 @@
-/* import './App.css'; */
-import Banner from './Banner'
-import Cart from './Cart'
+import '../styles/App.css';
+import Banner from './Banner';
+import Cart from './Cart';
 
 function App() {
   return (
--- jungle-house/src/components/Banner.js
+++ jungle-house/src/components/Banner.js
@@ -1,9 +1,21 @@
+import logo from '../assets/logo.png'
 import '../styles/Banner.css'
 
 function Banner() {
-    <div className='jh-banner'>
-        <h1>Jungle House</h1>
-    </div>
+    const title = 'Jungle House'
+    return (
+        <div className='jh-banner'>
+            <img src= {logo} alt='Jungle House' className='jh-logo'/>
+            <h1 className='jh-title'>{title}</h1>
+     {/*    <div style={{
+            color: 'black',
+            textAlign: 'right',
+            padding: 32,
+            borderBottom: 'solid 3px black'
+        }}> */}
+        
+        </div>
+    )
 }
 
 export default Banner
